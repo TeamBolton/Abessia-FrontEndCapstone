368fdb68371eb60cd802bacd7fcd4f56281f0898
Abessia <rebecca.e.wiegel@gmail.com>
2020-11-12 18:46:50 -0700

    Jest set up in client and server files with config in package.json

---
+++ client/client.spec.js
@@ -0,0 +1,7 @@
+const Jest = require('jest');
+
+describe('Jest: ', () => {
+  it('loads properly in client.spec.js', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ server/server.spec.js
@@ -0,0 +1,7 @@
+const Jest = require('jest');
+
+describe('Jest: ', () => {
+  it('loads properly in server.spec.js', () => {
+    expect(true).toEqual(true);
+  });
+});
