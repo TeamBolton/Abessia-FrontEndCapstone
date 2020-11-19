9a1d0e0d21321fb3d3dda2f4ad74df2bd9af84ad
Abessia <rebecca.e.wiegel@gmail.com>
2020-11-18 21:16:14 -0700

    Implemented Grid Shape for front-end

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Axios from 'axios';
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto auto;
+  gird-template-rows: auto auto auto auto auto auto auto auto auto;
+  grid-gap: 2px;
+  background: cornflowerblue;
+`;
+
+const Item = styled.div`
+  font-size: 18px;
+  text-align: center;
+  border: 1px solid;
+`;
+
+const MainImage = styled(Item)`
+  grid-area: main-image;
+  grid-column: 1 / 8;
+  grid-row: 1 / 8;
+  background: white;
+`;
+
+const LeftButton = styled(Item)`
+  grid-area: left-button;
+  grid-column: 1;
+  grid-row: 8;
+  background: white;
+`;
+
+const RightButton = styled(Item)`
+  grid-area: right-button;
+  grid-column: 7;
+  grid-row: 8;
+  background: white;
+`;
+
+const PreviewImageBar = styled(Item)`
+  grid-area: preview-image-bar
+  grid-column: 2 / 6;
+  grid-row: 8;
+`;
+
+const InfoBar = styled(Item)`
+  grid-area: info-bar;
+  grid-column: 1 / 5;
+  grid-row: 9;
+`;
+
+const Blank = styled(Item)`
+  grid-area: blank;
+  grid-column: 8 / 13;
+  grid-row: 8 / 10;
+  border: 0px;
+`;
+
+const ZoomImage = styled(Item)`
+  grid-area: zoom-image;
+  grid-column: 8 / 13;
+  grid-row: 1 / 7;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -54,7 +115,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <h1>Hello World!</h1>
+        <Grid class="grid-container">
+          <MainImage class="grid-item">Main Image</MainImage>
+          <ZoomImage class="grid-item">Zoom Image</ZoomImage>
+          <LeftButton class="grid-item">Left</LeftButton>
+          <PreviewImageBar class="grid-item">Preview Images</PreviewImageBar>
+          <RightButton class="grid-item">Right</RightButton>
+          <Blank class="grid-item"></Blank>
+          <InfoBar class="grid-item">Information</InfoBar>
+        </Grid>
+      </div>
     );
   }
 }
